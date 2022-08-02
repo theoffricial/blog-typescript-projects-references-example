78770860d6ecbda6fb50648b55b28b92aa2bad44
unicop <its.op.the.unicorn@gmail.com>
2022-08-02 09:30:42 +0300

    add chalk

---
+++ build/wonderland/initial-wonderland.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+var chalk_1 = tslib_1.__importDefault(require("chalk"));
+var creatures_1 = require("../creatures");
+var formatting_1 = require("../formatting");
+var cheshire = new creatures_1.CheshireCat();
+var whiteRabbit = new creatures_1.WhiteRabbit();
+function initialWonderland() {
+    console.log(chalk_1.default.bgBlue(chalk_1.default.whiteBright("This is Wonderland!")));
+    console.log(chalk_1.default.bgMagenta("Now all creatures introduce themselves:"));
+    console.log(chalk_1.default.bgGreen(chalk_1.default.black("1. ".concat((0, formatting_1.formatNameAndSound)(cheshire)))));
+    console.log(chalk_1.default.bgWhite(chalk_1.default.black("2. ".concat((0, formatting_1.formatNameAndSound)(whiteRabbit)))));
+}
+initialWonderland();
---
+++ src/wonderland/initial-wonderland.ts
@@ -0,0 +1,15 @@
+import chalk from 'chalk'
+import {CheshireCat,WhiteRabbit} from '../creatures'
+import {formatNameAndSound} from '../formatting';
+const cheshire = new CheshireCat()
+const whiteRabbit = new WhiteRabbit()
+
+function initialWonderland() {
+    console.log(chalk.bgBlue(chalk.whiteBright("This is Wonderland!")))
+    console.log(chalk.bgMagenta("Now all creatures introduce themselves:"))
+    // creatures say hello
+    console.log(chalk.bgGreen(chalk.black(`1. ${formatNameAndSound(cheshire)}`)))
+    console.log(chalk.bgWhite(chalk.black(`2. ${formatNameAndSound(whiteRabbit)}`)))
+}
+
+initialWonderland()
